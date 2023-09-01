135639f58ed2cd0a076c379456b03f5b2a1eb6d2
Maggie <mengtian.thu@gmail.com>
2023-09-01 03:00:56 -0700

    add loading

---
+++ react-app/src/components/LoadingPage/LoadingPage.js
@@ -0,0 +1,11 @@
+import "./LoadingPage.css";
+
+function LoadingPage() {
+  return (
+    <div className="spinner">
+      {/* <img src={`../../images/spin.gif`} alt="Loading in progress"></img> */}
+    </div>
+  );
+}
+
+export default LoadingPage;
